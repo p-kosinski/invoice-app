b7b56ae729f661c1acef5168e22ec5bee473ecfa
p-kosinski <kosinskipiotr.98@gmail.com>
2022-07-10 18:04:01 +0200

    Add redux store & invoices reducer

---
+++ src/redux/invoicesSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from './store';
+
+export type anyArray = any[];
+
+export interface LoadingState {
+  active: boolean;
+  error: boolean;
+};
+
+interface InvoicesState {
+  loading: LoadingState;
+  data: anyArray;
+};
+
+const initialState: InvoicesState = {
+  loading: {
+    active: false,
+    error: false,
+  },
+  data: [],
+};
+
+export const invoicesSlice = createSlice({
+  name: 'invoices',
+  initialState,
+  reducers: {
+    setLoadingActive: (state, action: PayloadAction<boolean>) => {
+      state.loading.active = action.payload;
+    },
+    setLoadingError: (state, action: PayloadAction<boolean>) => {
+      state.loading.error = action.payload;
+    },
+    setData: (state, action: PayloadAction<anyArray>) => {
+      state.data = action.payload;
+    }
+  }
+});
+
+export const {
+  setLoadingActive,
+  setLoadingError,
+  setData
+} = invoicesSlice.actions;
+
+export const selectInvoicesData = (state: RootState) => state.invoices.data;
+export const selectInvoicesLoading = (state: RootState) => state.invoices.loading;
+
+export default invoicesSlice.reducer;
---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import invoicesReducer from './invoicesSlice';
+
+const store = configureStore({
+  reducer: {
+    invoices: invoicesReducer,
+  }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
