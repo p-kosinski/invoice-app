1c028a484e8578daf306c2a351d252065202aa27
p-kosinski <kosinskipiotr.98@gmail.com>
2022-07-10 20:47:13 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 import store from './redux/store';
 
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import Invoices from './components/views/Invoices/Invoices';
+import InvoiceEdit from './components/views/InvoiceEdit/InvoiceEdit';
+import Invoice from './components/views/Invoice/Invoice';
+
 const App = () => (
   <Provider store={store}>
-    <div></div>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path='/' element={<Invoices />}>
+            <Route path='new' element={<InvoiceEdit newInvoice />} />
+          </Route>
+          <Route path='/:id' element={<Invoice />}>
+            <Route path='edit' element={<InvoiceEdit />} />
+          </Route>
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   </Provider>
 );
 
