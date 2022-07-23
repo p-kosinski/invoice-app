cf9611b574f2c0e53f95d219ed5c9111bd74139c
p-kosinski <kosinskipiotr.98@gmail.com>
2022-07-23 18:04:25 +0200

    Add TextField component (without styling)

--- src/components/common/TextField/TextField.tsx
+++ src/components/common/TextField/TextField.tsx
@@ -1,11 +1,37 @@
-import Styled from './Styled';
-
 type Props = {
-  
+  name: string;
+  label?: string;
+  ariaLabel?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  value: string;
+  onChange: (value: string) => any;
 };
 
-const Typography: React.FC<Props> = ({  }) => (
-  <></>
+const TextField: React.FC<Props> = ({
+  name,
+  label,
+  ariaLabel,
+  placeholder,
+  required,
+  disabled,
+  value,
+  onChange
+}) => (
+  <label>
+    {label}
+    <input
+      type='text'
+      name={name}
+      aria-label={ariaLabel}
+      placeholder={placeholder}
+      required={required}
+      disabled={disabled}
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
+    />
+  </label>
 );
 
-export default Typography;
+export default TextField;
