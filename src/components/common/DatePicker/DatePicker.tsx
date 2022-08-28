83b8a20624906b34d40b44ddea404b7cce3aa1d0
p-kosinski <kosinskipiotr.98@gmail.com>
2022-08-28 11:45:00 +0200

    Add calendar closing on click away in DatePicker component

--- src/components/common/DatePicker/DatePicker.tsx
+++ src/components/common/DatePicker/DatePicker.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
+import ClickAwayListener from 'react-click-away-listener';
 
 import {
   changeDateMonth,
@@ -56,67 +57,69 @@ const DatePicker: React.FC<Props> = ({
   }, [shownDate]);
 
   return (
-    <Styled.Wrapper>
-      <Styled.Label id={name} htmlFor={name}>
-        <Typography variant='body1' element='span'>
-          {label}
-        </Typography>
-      </Styled.Label>
-      <Styled.Input
-        id={name}
-        name={name}
-        aria-label={label}
-        aria-haspopup='true'
-        aria-expanded={calendarOpen}
-        disabled={disabled}
-        onClick={() => setCalendarOpen(!calendarOpen)}
-      >
-        <Styled.InputWrapper>
-          <Styled.DateWrapper $disabled={disabled}>
-            {getSelectedDateString()}
-          </Styled.DateWrapper>
-          <CalendarIcon />
-        </Styled.InputWrapper>
-      </Styled.Input>
-      <Styled.PopupCard
-        aria-hidden={!calendarOpen}
-        $visible={calendarOpen}
-      >
-        <Styled.MonthPicker>
-          <Styled.MonthPickerButton onClick={handleArrowClick(false)}>
-            <LeftArrowIcon />
-          </Styled.MonthPickerButton>
-          <Typography variant='h4' element='span'>
-            {getShownDateString()}
+    <ClickAwayListener onClickAway={() => setCalendarOpen(false)}>
+      <Styled.Wrapper>
+        <Styled.Label id={name} htmlFor={name}>
+          <Typography variant='body1' element='span'>
+            {label}
           </Typography>
-          <Styled.MonthPickerButton onClick={handleArrowClick(true)}>
-            <RightArrowIcon />
-          </Styled.MonthPickerButton>
-        </Styled.MonthPicker>
-        <Styled.Calendar>
-          {rows.map((cells, rowIndex) => (
-            <Styled.CalendarRow key={rowIndex}>
-              {cells.map(({ text, value, inShownMonth }, cellIndex) => (
-                <Styled.CalendarCell
-                  key={`${text} - ${cellIndex}`}
-                  $selected={checkIfCellDateIsSelected(value, selectedDate)}
-                  $inShownMonth={inShownMonth}
-                  disabled={!inShownMonth}
-                  onClick={() => {
-                    onChange(dayjs.utc(value).toISOString());
-                    setCalendarOpen(false);
-                  }}
-                >
-                  <Typography variant='h4' element='span'>
-                    {text}
-                  </Typography>
-                </Styled.CalendarCell>
-              ))}
-            </Styled.CalendarRow>
-          ))}
-        </Styled.Calendar>
-      </Styled.PopupCard>
-    </Styled.Wrapper>
+        </Styled.Label>
+        <Styled.Input
+          id={name}
+          name={name}
+          aria-label={label}
+          aria-haspopup='true'
+          aria-expanded={calendarOpen}
+          disabled={disabled}
+          onClick={() => setCalendarOpen(!calendarOpen)}
+        >
+          <Styled.InputWrapper>
+            <Styled.DateWrapper $disabled={disabled}>
+              {getSelectedDateString()}
+            </Styled.DateWrapper>
+            <CalendarIcon />
+          </Styled.InputWrapper>
+        </Styled.Input>
+        <Styled.PopupCard
+          aria-hidden={!calendarOpen}
+          $visible={calendarOpen}
+        >
+          <Styled.MonthPicker>
+            <Styled.MonthPickerButton onClick={handleArrowClick(false)}>
+              <LeftArrowIcon />
+            </Styled.MonthPickerButton>
+            <Typography variant='h4' element='span'>
+              {getShownDateString()}
+            </Typography>
+            <Styled.MonthPickerButton onClick={handleArrowClick(true)}>
+              <RightArrowIcon />
+            </Styled.MonthPickerButton>
+          </Styled.MonthPicker>
+          <Styled.Calendar>
+            {rows.map((cells, rowIndex) => (
+              <Styled.CalendarRow key={rowIndex}>
+                {cells.map(({ text, value, inShownMonth }, cellIndex) => (
+                  <Styled.CalendarCell
+                    key={`${text} - ${cellIndex}`}
+                    $selected={checkIfCellDateIsSelected(value, selectedDate)}
+                    $inShownMonth={inShownMonth}
+                    disabled={!inShownMonth}
+                    onClick={() => {
+                      onChange(dayjs.utc(value).toISOString());
+                      setCalendarOpen(false);
+                    }}
+                  >
+                    <Typography variant='h4' element='span'>
+                      {text}
+                    </Typography>
+                  </Styled.CalendarCell>
+                ))}
+              </Styled.CalendarRow>
+            ))}
+          </Styled.Calendar>
+        </Styled.PopupCard>
+      </Styled.Wrapper>
+    </ClickAwayListener>
   );
 };
 
