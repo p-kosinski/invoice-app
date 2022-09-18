7dc57f8ff97c0dfd4c25d87bba49e76a2dcb6c88
p-kosinski <kosinskipiotr.98@gmail.com>
2022-09-18 19:11:44 +0200

    Add loading invoices data on mount in Invoices view

--- src/components/views/Invoices/Invoices.tsx
+++ src/components/views/Invoices/Invoices.tsx
@@ -1,12 +1,34 @@
+import { useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
-const Invoices: React.FC = () => (
-  <div>
-    <h2>Invoices view</h2>
-    <Link to='/123'>Invoice 123</Link>
-    <Link to='/new'>New Invoice</Link>
-    <Outlet />
-  </div>
-);
+import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHooks';
+import {
+  fetchInvoicesData,
+  selectInvoicesData,
+  selectInvoicesLoadingState
+} from '../../../redux/invoicesSlice';
+import type { InvoicesData, LoadingState } from '../../../redux/invoicesSlice';
+
+import Container from '../../layout/Container/Container';
+
+const Invoices: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const invoices: InvoicesData = useAppSelector(selectInvoicesData);
+  const invoicesLoading: LoadingState = useAppSelector(selectInvoicesLoadingState);
+
+  useEffect(() => {
+    !invoices.length && dispatch(fetchInvoicesData());
+  }, []);
+
+  return (
+    <Container>
+      <h2>Invoices view</h2>
+      <Link to='/123'>Invoice 123</Link>
+      <Link to='/new'>New Invoice</Link>
+      <Outlet />
+    </Container>
+  );
+};
 
 export default Invoices;
