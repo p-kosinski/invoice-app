1bb052348887a9b894c8b5bdeb7312b523f279c0
p-kosinski <kosinskipiotr.98@gmail.com>
2022-09-19 14:12:41 +0200

    Fix theme background color transition in Card component

--- src/components/common/Card/Styled.tsx
+++ src/components/common/Card/Styled.tsx
@@ -4,12 +4,14 @@ const CardDiv = styled.div(({ theme }) => css`
   background-color: ${theme.colors.backgrounds.card};
   box-shadow: 0px 10px 10px -10px hsla(231, 38%, 45%, 0.1);
   border-radius: 8px;
+  transition: background-color ${theme.transitionDuration} ease-in-out;
 `);
 
 const CardArticle = styled.article(({ theme }) => css`
   background-color: ${theme.colors.backgrounds.card};
   box-shadow: 0px 10px 10px -10px hsla(231, 38%, 45%, 0.1);
   border-radius: 8px;
+  transition: background-color ${theme.transitionDuration} ease-in-out;
 `);
 
 const Styled = {
