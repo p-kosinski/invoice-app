9da6879bbd8b1253fa3fd59991357384c18648a4
p-kosinski <kosinskipiotr.98@gmail.com>
2022-09-25 13:48:41 +0200

    Fix page background color

--- src/components/layout/MainLayout/Styled.ts
+++ src/components/layout/MainLayout/Styled.ts
@@ -2,8 +2,6 @@ import styled, { css } from 'styled-components';
 
 const Main = styled.main(({ theme }) => css`
   padding-top: 72px;
-  background-color: ${theme.colors.backgrounds.page};
-  transition: background-color ${theme.transitionDuration} ease-in-out;
 
   @media only screen and (min-width: ${theme.breakpoints.lg}) {
     padding-top: 0;
