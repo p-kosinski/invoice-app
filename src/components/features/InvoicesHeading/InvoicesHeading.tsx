330e57914ad303ccef0492cd6216ed8d368abb6d
p-kosinski <kosinskipiotr.98@gmail.com>
2022-09-29 17:13:33 +0200

    Fix 'No total invoices' bug in InvoicesHeading

--- src/components/features/InvoicesHeading/InvoicesHeading.tsx
+++ src/components/features/InvoicesHeading/InvoicesHeading.tsx
@@ -1,30 +1,45 @@
 import { useAppSelector } from '../../../hooks/reduxHooks';
 import { selectStatusFilters } from '../../../redux/invoicesViewSlice';
-import { selectInvoicesFilteredByStatus } from '../../../redux/invoicesSlice';
+import {
+  selectInvoicesData,
+  selectInvoicesLoadingState,
+  selectInvoicesFilteredByStatus
+} from '../../../redux/invoicesSlice';
 import type { StatusFiltersArray } from '../../../redux/invoicesViewSlice';
-import type { InvoicesData, Status } from '../../../redux/invoicesSlice';
+import type { InvoicesData, LoadingState, Status } from '../../../redux/invoicesSlice';
 
 import Typography from '../../common/Typography/Typography';
+import Skeleton from '../../common/Skeleton/Skeleton';
 
 import Styled from './Styled';
 
 const InvoicesHeading: React.FC = () => {
   const statusFilters: StatusFiltersArray = useAppSelector(selectStatusFilters);
+  const invoices: InvoicesData = useAppSelector(selectInvoicesData);
+  const invoicesLoading: LoadingState = useAppSelector(selectInvoicesLoadingState);
   const filteredInvoices: InvoicesData = useAppSelector((state) =>
     selectInvoicesFilteredByStatus(state, statusFilters)
   );
 
-  type TextContentAdjective = Status | 'filtered' | 'total';
+  type TextContentAdjective = '' | Status | 'filtered' | 'total';
 
   const getTextContentAdjective = (
-    filtersArray: StatusFiltersArray
+    filtersArray: StatusFiltersArray,
+    invoices: InvoicesData
   ): TextContentAdjective => {
-    if (filtersArray.length === 1) {
-      return filtersArray[0];
-    } else if (filtersArray.length && filtersArray.length > 1) {
-      return 'filtered';
-    } else {
-      return 'total';
+    switch(true) {
+      case !invoices.length:
+        return '';
+      case filtersArray.length === 1:
+        return filtersArray[0];
+      case filtersArray.length > 1:
+        return 'filtered';
+      case filtersArray.length > 1:
+        return 'filtered';
+      case !filtersArray.length:
+        return 'total';
+      default:
+        return '';
     }
   };
 
@@ -33,13 +48,18 @@ const InvoicesHeading: React.FC = () => {
       <Typography variant='h1'>
         Invoices
       </Typography>
-      <Typography variant='body1' element='p'>
-        {filteredInvoices.length ? filteredInvoices.length : 'No'}
-        <Styled.SpanHiddenOnMobile>
-          &nbsp;{getTextContentAdjective(statusFilters)}
-        </Styled.SpanHiddenOnMobile>
-        &nbsp;{filteredInvoices.length === 1 ? 'invoice' : 'invoices'}
-      </Typography>
+      {invoicesLoading.active ?
+        <Skeleton variant='text' height='12px' width='75px' />
+        :
+        <Typography variant='body1' element='p'>
+          {filteredInvoices.length ? filteredInvoices.length : 'No'}
+          <Styled.SpanHiddenOnMobile>
+            {invoices.length ? ' ' : ''}
+            {getTextContentAdjective(statusFilters, invoices)}
+          </Styled.SpanHiddenOnMobile>
+          &nbsp;{filteredInvoices.length === 1 ? 'invoice' : 'invoices'}
+        </Typography>
+      }
     </Styled.Wrapper>
   );
 };
