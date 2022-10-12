44666cacbaa4a3a41cf7b62992ac308c49831bbc
p-kosinski <kosinskipiotr.98@gmail.com>
2022-10-12 18:41:04 +0200

    Add changing invoice status functionality in InvoiceActionButtons component

--- src/components/features/InvoiceActionButtons/InvoiceActionButtons.tsx
+++ src/components/features/InvoiceActionButtons/InvoiceActionButtons.tsx
@@ -1,33 +1,58 @@
+import { useParams } from 'react-router-dom';
+
+import { useAppSelector, useAppDispatch } from '../../../hooks/reduxHooks';
+import {
+  selectInvoiceStatusById,
+  changeInvoiceStatus
+} from '../../../redux/invoicesSlice';
+import type { Status } from '../../../redux/invoicesSlice';
+
+import { assertNotUndefined } from '../../../utils/typeUtils';
+
 import Container from '../../layout/Container/Container';
+import EditInvoiceLink from '../../common/EditInvoiceLink/EditInvoiceLink';
 import Button from '../../common/Button/Button';
 
 import Styled from './Styled';
 
-const InvoiceActionButtons: React.FC = () => (
-  <Styled.Toolbar>
-    <Container>
-      <Styled.Wrapper>
-        <Button
-          variant='edit'
-          onClick={() => console.log('"Edit" button was clicked')}
-        >
-          Edit
-        </Button>
-        <Button
-          variant='delete'
-          onClick={() => console.log('"Delete" button was clicked')}
-        >
-          Delete
-        </Button>
-        <Button
-          variant='primary'
-          onClick={() => console.log('"Mark as paid" button was clicked')}
-        >
-          Mark as paid
-        </Button>
-      </Styled.Wrapper>
-    </Container>
-  </Styled.Toolbar>
-);
+const InvoiceActionButtons: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const { id } = useParams<{ id?: string }>();
+
+  assertNotUndefined(id);
+
+  const status: Status = useAppSelector((state) =>
+    selectInvoiceStatusById(state, id)
+  );
+
+  const changeInvoiceStatusToPaid = () => {
+    dispatch(changeInvoiceStatus({ id: id, newStatus: 'paid' }))
+  };
+
+  return (
+    <Styled.Toolbar>
+      <Container>
+        <Styled.Wrapper>
+          <EditInvoiceLink />
+          <Button
+            variant='delete'
+            onClick={() => console.log('"Delete" button was clicked')}
+          >
+            Delete
+          </Button>
+          {status === 'pending' &&
+            <Button
+              variant='primary'
+              onClick={() => changeInvoiceStatusToPaid()}
+            >
+              Mark as paid
+            </Button>
+          }
+        </Styled.Wrapper>
+      </Container>
+    </Styled.Toolbar>
+  );
+};
 
 export default InvoiceActionButtons;
