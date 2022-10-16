122963251b8dbf24ab952793dcddea29ff288d28
p-kosinski <kosinskipiotr.98@gmail.com>
2022-10-16 14:47:42 +0200

    Fix Toolbar's box-shadow in InvoiceActionButtons component

--- src/components/features/InvoiceActionButtons/Styled.ts
+++ src/components/features/InvoiceActionButtons/Styled.ts
@@ -7,7 +7,7 @@ const Toolbar = styled.div(({ theme }) => css`
   width: 100%;
   padding: 24px 0;
   background-color: ${theme.colors.backgrounds.card};
-  box-shadow: 0px 10px 10px -10px hsla(231, 38%, 45%, 0.1);
+  box-shadow: 0px -10px 10px -5px hsla(231, 38%, 45%, 0.1);
   transition: background-color ${theme.transitionDuration} ease-in-out;
 
   @media only screen and (min-width: ${theme.breakpoints.sm}) {
