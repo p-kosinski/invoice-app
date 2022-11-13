f10f99e67de6b2e7176142db49866c20e5f5c14e
p-kosinski <kosinskipiotr.98@gmail.com>
2022-11-13 14:01:00 +0100

    Add IssueDate component

--- src/components/features/InvoiceForm/IssueDate/IssueDate.tsx
+++ src/components/features/InvoiceForm/IssueDate/IssueDate.tsx
@@ -1,19 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 
-import { parseDateToLocaleString } from '../../../../utils/dateUtils';
+import { useAppSelector, useAppDispatch } from '../../../../hooks/reduxHooks';
+
+import {
+  selectFormIssueDate,
+  setFormIssueDate
+} from '../../../../redux/invoicesViewSlice';
 
 import DatePicker from '../../../common/DatePicker/DatePicker';
 
 import Styled from '../Styled';
 
 const IssueDate: React.FC = () => {
-  const [date, setDate] = useState('');
+  const dispatch = useAppDispatch();
+
+  const date: string = useAppSelector(selectFormIssueDate);
+
+  const setDate = (newValue: string) => {
+    dispatch(setFormIssueDate(newValue));
+  };
 
   useEffect(() => {
-    setDate(parseDateToLocaleString(dayjs.utc()));
+    setDate(dayjs.utc().toISOString());
   }, []);
   
   return (
