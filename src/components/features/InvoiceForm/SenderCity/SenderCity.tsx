2a9a59c7a780fb55ae97cccf71a8623b03a20bd2
p-kosinski <kosinskipiotr.98@gmail.com>
2022-11-13 13:34:12 +0100

    Add SenderCity component

--- src/components/features/InvoiceForm/SenderCity/SenderCity.tsx
+++ src/components/features/InvoiceForm/SenderCity/SenderCity.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react';
+import { useAppSelector, useAppDispatch } from '../../../../hooks/reduxHooks';
+
+import {
+  selectFormSenderCity,
+  setFormSenderCity
+} from '../../../../redux/invoicesViewSlice';
 
 import TextField from '../../../common/TextField/TextField';
 
 import Styled from '../Styled';
 
 const SenderCity: React.FC = () => {
-  const [senderCity, setSenderCity] = useState('');
+  const dispatch = useAppDispatch();
+
+  const senderCity: string = useAppSelector(selectFormSenderCity);
+
+  const setSenderCity = (newValue: string) => {
+    dispatch(setFormSenderCity(newValue));
+  };
   
   return (
     <Styled.CityWrapper>
