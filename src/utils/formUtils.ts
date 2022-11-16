06a3296c026337189abe76e5aa16f3067c75dea7
p-kosinski <kosinskipiotr.98@gmail.com>
2022-11-16 19:29:49 +0100

    Add prepareItemsData function in formUtils

--- src/utils/formUtils.ts
+++ src/utils/formUtils.ts
@@ -2,6 +2,9 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 
+import type { FormItemsArray } from '../redux/invoiceFormSlice';
+import type { ItemsArray, Item } from '../redux/invoicesSlice';
+
 export const generateId = (): string => {
   const generateRandomString = (characters: string, length: number): string => {
     let result = '';
@@ -38,4 +41,38 @@ export const calculatePaymentDue = (
   daysForPayment: number
 ): string => {
   return dayjs.utc(issueDate).add(daysForPayment, 'day').format('YYYY-MM-DD');
+};
+
+export interface ItemsData {
+  itemsArray: ItemsArray;
+  itemsTotal: number;
+};
+
+export const prepareItemsData = (formItems: FormItemsArray): ItemsData => {
+  let itemsArray: ItemsArray = [];
+  let itemsTotal = 0;
+
+  for (let formItem of formItems) {
+    const { name, quantity, price } = formItem;
+
+    const parsedQuantity = parseInt(quantity);
+    const parsedPrice = parseFloat(price);
+
+    const itemData: Item = {
+      name: name,
+      quantity: parsedQuantity,
+      price: parsedPrice,
+      total: parsedQuantity * parsedPrice
+    };
+
+    itemsTotal += itemData.total;
+    itemsArray.push(itemData);
+  }
+
+  const itemsData: ItemsData = {
+    itemsArray: itemsArray,
+    itemsTotal: itemsTotal
+  };
+
+  return itemsData;
 };
