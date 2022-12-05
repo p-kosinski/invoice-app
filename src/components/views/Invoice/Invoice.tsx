cdadffbffa1b9c450dc7850073ebd8672b679e54
p-kosinski <kosinskipiotr.98@gmail.com>
2022-12-05 20:28:04 +0100

    Change in Invoice component - display EditInvoice component in a Drawer

--- src/components/views/Invoice/Invoice.tsx
+++ src/components/views/Invoice/Invoice.tsx
@@ -1,26 +1,39 @@
+import { useAppSelector } from '../../../hooks/reduxHooks';
+
+import { selectDrawerOpen } from '../../../redux/invoiceViewSlice';
+
 import Container from '../../layout/Container/Container';
 import GoBackButton from '../../common/GoBackButton/GoBackButton';
 import InvoiceInfo from '../../features/InvoiceInfo/InvoiceInfo';
 import InvoiceStatusCard from '../../features/InvoiceStatusCard/InvoiceStatusCard';
 import InvoiceActionButtons from '../../features/InvoiceActionButtons/InvoiceActionButtons';
 import DeletionConfirmDialog from '../../features/DeletionConfirmDialog/DeletionConfirmDialog';
+import Drawer from '../../layout/Drawer/Drawer';
+import EditInvoice from '../../features/EditInvoice/EditInvoice';
 
 import Styled from './Styled';
 
-const Invoice: React.FC = () => (
-  <>
-    <DeletionConfirmDialog />
-    <Container>
-      <Styled.GoBackLinkWrapper>
-        <GoBackButton />
-      </Styled.GoBackLinkWrapper>
-      <InvoiceStatusCard />
-      <Styled.InvoiceInfoWrapper>
-        <InvoiceInfo />
-      </Styled.InvoiceInfoWrapper>
-    </Container>
-    <InvoiceActionButtons />
-  </>
-);
+const Invoice: React.FC = () => {
+  const drawerOpen: boolean = useAppSelector(selectDrawerOpen);
+
+  return (
+    <>
+      <Drawer open={drawerOpen}>
+        <EditInvoice />
+      </Drawer>
+      <DeletionConfirmDialog />
+      <Container>
+        <Styled.GoBackLinkWrapper>
+          <GoBackButton />
+        </Styled.GoBackLinkWrapper>
+        <InvoiceStatusCard />
+        <Styled.InvoiceInfoWrapper>
+          <InvoiceInfo />
+        </Styled.InvoiceInfoWrapper>
+      </Container>
+      <InvoiceActionButtons />
+    </>
+  );
+};
 
 export default Invoice;
