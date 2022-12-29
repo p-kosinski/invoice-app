ca8f2aa37ada7b7bd144fadad60d31d149ca774c
p-kosinski <kosinskipiotr.98@gmail.com>
2022-12-29 19:15:12 +0100

    Fix mobile toolbar overlapping main content in invoice view

--- src/components/features/InvoiceInfo/Styled.ts
+++ src/components/features/InvoiceInfo/Styled.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 
 const Wrapper = styled.div(({ theme }) => css`
   padding: 24px;
+  margin-bottom: calc(96px + 24px);
 
   @media only screen and (min-width: ${theme.breakpoints.sm}) {
     padding: 32px;
