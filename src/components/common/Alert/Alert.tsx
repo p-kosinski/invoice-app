0871be06506c5b7474962fddfb2d180f23363576
p-kosinski <kosinskipiotr.98@gmail.com>
2023-01-17 17:32:24 +0100

    Add Alert component

---
+++ src/components/common/Alert/Alert.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck,
+  faTriangleExclamation,
+  faXmark
+} from '@fortawesome/free-solid-svg-icons';
+
+import Typography from '../Typography/Typography';
+
+import Styled from './Styled';
+
+type Props = {
+  severity: 'success' | 'error';
+  closeFunc: () => void;
+  children?: ReactNode;
+};
+
+const Alert: React.FC<Props> = ({ severity, closeFunc, children }) => {
+  return (
+    <Styled.Alert
+      $severity={severity}
+      layout='position'
+      initial={{ opacity: 0, x: 200 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 200 }}
+      transition={{ duration: 0.15 }}
+    >
+      <Styled.HeadingWrapper>
+        {severity === 'success' &&
+          <FontAwesomeIcon icon={faCircleCheck} />
+        }
+        {severity === 'error' &&
+          <FontAwesomeIcon icon={faTriangleExclamation} />
+        }
+        <Styled.CloseButton onClick={() => closeFunc()}>
+          <FontAwesomeIcon icon={faXmark} />
+        </Styled.CloseButton>
+      </Styled.HeadingWrapper>
+      <Typography variant='h4' element='p'>
+        {children}
+      </Typography>
+    </Styled.Alert>
+  );
+};
+
+export default Alert;
---
+++ src/components/common/Alert/Styled.ts
@@ -0,0 +1,76 @@
+import styled, { css } from 'styled-components';
+import { motion } from 'framer-motion';
+
+type AlertProps = {
+  $severity: 'success' | 'error';
+};
+
+const Alert = styled(motion.div)<AlertProps>(({ theme, $severity }) => css`
+  padding: 16px 24px;
+  border-radius: 8px;
+  ${$severity === 'success' && `
+    border: 1px solid ${theme.colors.alerts.success};
+  `};
+  ${$severity === 'error' && `
+    border: 1px solid ${theme.colors.alerts.error};
+  `};
+  background-color: ${theme.colors.backgrounds.card};
+  color: ${theme.colors.text.main};
+  box-shadow: 0px 24px 24px 0px ${theme.colors.shadow.main};
+  z-index: 999;
+  transition:
+    background-color ${theme.transitionDuration} ease-in-out,
+    color ${theme.transitionDuration} ease-in-out,
+    box-shadow ${theme.transitionDuration} ease-in-out,
+    border ${theme.transitionDuration} ease-in-out;
+
+  p {
+    color: inherit;
+    margin-bottom: 0;
+  }
+
+  svg {
+    transition: color ${theme.transitionDuration} ease-in-out;
+    ${$severity === 'success' && `
+      color: ${theme.colors.alerts.success};
+    `};
+    ${$severity === 'error' && `
+      color: ${theme.colors.alerts.error};
+    `};
+    height: 20px;
+    width: 20px;
+  }
+`);
+
+const HeadingWrapper = styled.div(({ theme }) => css`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`);
+
+const CloseButton = styled.button(({ theme }) => css`
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  cursor: pointer;
+
+  svg {
+    transition: transform ${theme.transitionDuration} ease-in-out;
+  }
+
+  @media (hover: hover) {
+    &:hover {
+      svg {
+        transform: scale(1.25);
+      }
+    }
+  }
+`);
+
+const Styled = {
+  Alert: Alert,
+  HeadingWrapper: HeadingWrapper,
+  CloseButton: CloseButton
+};
+
+export default Styled;
