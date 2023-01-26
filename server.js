af3e4361fd63fe6d40f492ed054855862ca15788
p-kosinski <kosinskipiotr.98@gmail.com>
2023-01-26 16:49:20 +0100

    Change in server.js - serve dist folder

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('dist/db/db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: './dist',
+});
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
